e815fc1bba4ee9d53b37580fa9285176fafda18d
alasirimaya <alasirimaya@gmail.com>
2025-01-19 16:28:44 +0300

    Initial Commit

---
+++ movieApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movieApp
+//
+//  Created by maya alasiri  on 19/07/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movieApp/movieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieAppApp.swift
+//  movieApp
+//
+//  Created by maya alasiri  on 19/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct movieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
